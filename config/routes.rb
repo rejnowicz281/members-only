2524728cf1e65dd35f404ca508000182b5e25c4b
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-06 16:47:52 +0200

    Generate controllers for User and Post models

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:new, :create] do
+    resources :posts, only: [:index, :new, :create]
+  end
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
