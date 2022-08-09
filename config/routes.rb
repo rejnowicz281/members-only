79fb772b569a0a8dab306596ea67aa698d8f3077
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-09 15:33:32 +0200

    Change resources for the models in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:new, :create] do
-    resources :posts, only: [:index, :new, :create]
-  end
+  resources :posts, only: [:index, :new, :create]
 
   # Defines the root path route ("/")
   root "posts#index"
